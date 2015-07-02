770699dc9acaffc40a2c1fff566fb7e9007fe423
linusgiertta <linus-giertta@coudnine.se>
2015-07-02 15:51:04 +0200

    Add New.

---
+++ NancyTest/Modules/HomeModule.cs
@@ -0,0 +1,39 @@
+﻿using Nancy;
+using NancyTest.Objects;
+using Nancy.ModelBinding;
+
+namespace NancyTest.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters =>
+            {
+                var blogPost = new BlogPost
+                {
+                    Id = 1,
+                    Title = "From ASP.NET MVC to Nancy - Part 2",
+                    Content = "Lorem ipsum...",
+                    Tags = { "c#", "aspnetmvc", "nancy" }
+                };
+
+                return View["Index", blogPost];
+            };
+
+            Get["/new/"] = parameters =>
+            {
+                var blogPost = new BlogPost();
+
+                return View["New", blogPost];
+            };
+
+            Post["/new/"] = parameters =>
+            {
+                var blogPost = this.Bind<BlogPost>();
+                // Redirects the user to our Index action with a "status" value as a querystring.
+                return Response.AsRedirect("/?status=added&title=" + blogPost.Title);
+            };
+        }
+    }
+}
